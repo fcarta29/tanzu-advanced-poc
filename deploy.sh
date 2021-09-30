b19c6ab1091722f6c0dde3fc4160905127969b64
Frank Carta <fcarta@vmware.com>
2021-09-29 18:01:00 -0700

    Updates for workshop

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,10 @@
 
 cp -ur scenarios /opt/workshop/content/
 rm /opt/workshop/content/labs/testcase.md
+rm /opt/workshop/content/labs/testcase.md.in
+rm /opt/workshop/content/labs/setup.md
+
+testcasename=$(basename "$1")
+cp -u $1 /opt/workshop/content/labs/testcase.md.in
+cp -u scenarios/env/$testcasename /opt/workshop/content/labs/setup.md
 cp -u $1 /opt/workshop/content/labs/testcase.md
